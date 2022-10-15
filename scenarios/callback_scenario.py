2ff3e6cbe42e46f0c258a14c84bf10b890213252
Joshua J. Damanik <joshuajdmk@gmail.com>
2022-10-15 22:05:22 +0900

    modified message yaml

--- scenarios/callback_scenario.py
+++ scenarios/callback_scenario.py
@@ -24,6 +24,8 @@ class Vehicle():
         self.status_sub = self.node.create_subscription(VehicleStatus, '/vehicle%d/out/VehicleStatus' % id, self.status_callback, 10)
         self.odo_sub = self.node.create_subscription(VehicleOdometry, '/vehicle%d/out/VehicleOdometry' % id, self.odo_callback, 10)
 
+        self.move(100., 100., 10.)
+
     def home_callback(self, msg):
         print(msg)
 
@@ -34,15 +36,56 @@ class Vehicle():
         print(msg)
         # pass
 
+    def arm(self):
+        print("send ARM command")
+        arm_cmd = VehicleCommand()
+        arm_cmd.target_system = self.id
+        arm_cmd.command = 400
+        arm_cmd.param1 = 1.0
+        arm_cmd.confirmation = 0
+        arm_cmd.from_external = True
+        self.command.publish(arm_cmd)
+
+    def move(self, x, y, z):
+        print("send MOVE command")
+        arm_cmd = VehicleCommand()
+        arm_cmd.target_system = self.id
+        arm_cmd.command = 81
+        arm_cmd.param1 = 1.0
+        arm_cmd.param2 = 1.0
+        arm_cmd.param4 = 0.
+        arm_cmd.param5 = x
+        arm_cmd.param6 = y
+        arm_cmd.param7 = z
+        arm_cmd.confirmation = True
+        arm_cmd.from_external = True
+        self.command.publish(arm_cmd)
+
+    def set_position(self, waypoint):
+        print("send moving command")
+        move_cmd = VehicleCommand()
+        move_cmd.target_system = self.id
+        move_cmd.command = 192
+        move_cmd.param1 = -1.0
+        move_cmd.param2 = 1.0
+        move_cmd.param3 = 0.0
+        move_cmd.param4 = float('nan')
+        move_cmd.param5 = waypoint[0]
+        move_cmd.param6 = waypoint[1]
+        move_cmd.param7 = waypoint[2]
+        move_cmd.confirmation = True
+        move_cmd.from_external = True
+        self.command.publish(move_cmd)
+
 def main(args=None):
     start_time = time.time()
     rclpy.init(args=args)
 
     node = Node('px4_command_publisher')
 
-    vehicle1 = Vehicle(node, 1, [xy2latlon(150, 0, 5),
-                                 xy2latlon(150, 90, 5),
-                                 xy2latlon(240, 90, 5)])
+    vehicle1 = Vehicle(node, 1, [xy2latlon(150., 0., 5.),
+                                 xy2latlon(150., 90., 5.),
+                                 xy2latlon(240., 90., 5.)])
 
     exit_value = 0
     # rclpy.spin(scenario_test)
